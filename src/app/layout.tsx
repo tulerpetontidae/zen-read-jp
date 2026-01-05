5ff6aa51b2ef6611f36f34c30f5002b434b9db86
tulerpetontidae <artem@lomakin.com>
2026-01-05 14:40:45 -0800

    vercel analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Serif_JP, Noto_Sans_JP } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 
@@ -31,6 +33,8 @@ export default function RootLayout({
         className={`${notoSerifJP.variable} ${notoSansJP.variable} antialiased`}
       >
         <ThemeProvider>{children}</ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
